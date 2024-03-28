67fe8c7600fc02d9e8d223b50db25ef49ebee99b
Ateuluz <lucas.zulueta.w@gmail.com>
2024-03-28 11:26:38 -0300

    Armament Structure

--- src/main/scala/elements/armament.scala
+++ src/main/scala/elements/armament.scala
@@ -1,9 +1,44 @@
 package elements
 
+/**
+ *
+ */
 trait Weapon {
-  val weight: Int;
+  val name: String
+  val attack: Int
+  val weight: Int
+  var owner: Character
+
 }
 
-trait MagicalWeapon;
+/**
+ *
+ * @param magicAttack The attack for spells
+ */
+trait MagicalWeapon{
+  val magicAttak: Int
+}
 
-trait NonMagicalWeapon;
+/**
+ *
+ */
+trait NonMagicalWeapon
+
+/**
+ *
+ * @param name   The name of the Sword
+ * @param attack The attack value
+ * @param weight The Sword's weight
+ * @param owner  The Sword's owner
+ */
+class Sword(
+             val name: String,
+             val attack: Int,
+             val weight: Int,
+             var owner: Character,
+           ) extends NonMagicalWeapon {
+
+  def this(name: String, attack: Int, weight: Int) = {
+    this(name, attack, weight, null)
+  }
+}
