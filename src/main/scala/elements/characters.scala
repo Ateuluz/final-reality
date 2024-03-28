d526fc5477f32ac624d827aea770bccccdc2b38d
Ateuluz <lucas.zulueta.w@gmail.com>
2024-03-28 00:49:52 -0300

    Beggining documentation

--- src/main/scala/elements/characters.scala
+++ src/main/scala/elements/characters.scala
@@ -1,14 +1,22 @@
 package elements
 
-trait WeaponBearer {
-  var weapon: Weapon;
-}
+/** Define the basic traits of a character
+ *
+ *
+ * @param name    The name of the character
+ * @param hp      The health points
+ * @param defense The resistance to damage
+ * @param weight  The weight of the character
+ * @param magic   The capacity to cast magic
+ * @param weapon  The slot for equipping a weapon
+ */
 trait Character {
   val name: String;
   var hp: Int;
   val defense: Int;
   val weight: Int;
   val magic: Boolean;
+  var weapon: Weapon;
 }
 
 trait NonMagicalCharacter extends Character {
