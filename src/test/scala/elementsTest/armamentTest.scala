f4be1587e7d9c818e3b09982e94fe337f61f2717
Ateuluz <lucas.zulueta.w@gmail.com>
2024-03-28 11:43:10 -0300

    Armament Test Structure

--- src/test/scala/elementsTest/armamentTest.scala
+++ src/test/scala/elementsTest/armamentTest.scala
@@ -1,1 +1,18 @@
 package elementsTest
+
+import elements.Sword
+
+class SwordTest extends munit.FunSuite() {
+  var tstSbjt1: Sword = _
+  var tstSbjt2: Sword = _
+
+  override def beforeEach(context: BeforeEach): Unit = {
+    //super.beforeEach(context)
+    tstSbjt1 = new Sword("Excal", 5, 10)
+    tstSbjt2 = new Sword("Claym", 20, 30)
+  }
+
+  test("???") {
+    val expected = ???
+  }
+}
