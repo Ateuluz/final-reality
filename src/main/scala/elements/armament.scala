d880dc9297b1ef48eb910793e26df6d55308e675
Ateuluz <lucas.zulueta.w@gmail.com>
2024-03-27 23:37:34 -0300

    Starting with characters, yet to test

--- src/main/scala/elements/armament.scala
+++ src/main/scala/elements/armament.scala
@@ -1,1 +1,9 @@
 package elements
+
+trait Weapon {
+  val weight: Int;
+}
+
+trait MagicalWeapon;
+
+trait NonMagicalWeapon;
--- src/main/scala/elements/characters.scala
+++ src/main/scala/elements/characters.scala
@@ -1,1 +1,30 @@
 package elements
+
+trait WeaponBearer {
+  var weapon: Weapon;
+}
+trait Character {
+  val name: String;
+  var hp: Int;
+  val defense: Int;
+  val weight: Int;
+  val magic: Boolean;
+}
+
+trait NonMagicalCharacter extends Character {
+  val magic: Boolean = false;
+}
+
+trait MagicalCharacter extends Character {
+  val magic: Boolean = true;
+}
+
+class Paladin extends NonMagicalCharacter();
+
+class Warrior extends NonMagicalCharacter();
+
+class Ninja extends NonMagicalCharacter();
+
+class DarkMage extends MagicalCharacter();
+
+class WhiteMage extends MagicalCharacter();
