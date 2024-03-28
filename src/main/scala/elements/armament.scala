9191e9020b6c26f8e8db33a57505a4182c22ab1c
Ateuluz <lucas.zulueta.w@gmail.com>
2024-03-28 14:52:12 -0300

    Armament Test Structure

--- src/main/scala/elements/armament.scala
+++ src/main/scala/elements/armament.scala
@@ -15,14 +15,14 @@ trait Weapon {
  *
  * @param magicAttack The attack for spells
  */
-trait MagicalWeapon{
-  val magicAttak: Int
+trait MagicalWeapon extends Weapon {
+  val magicAttack: Int
 }
 
 /**
  *
  */
-trait NonMagicalWeapon
+trait NonMagicalWeapon extends Weapon
 
 /**
  *
@@ -40,5 +40,85 @@ class Sword(
 
   def this(name: String, attack: Int, weight: Int) = {
     this(name, attack, weight, null)
+  }
+}
+
+/**
+ *
+ * @param name   The name of the Sword
+ * @param attack The attack value
+ * @param weight The Sword's weight
+ * @param owner  The Sword's owner
+ */
+class Axe(
+           val name: String,
+           val attack: Int,
+           val weight: Int,
+           var owner: Character,
+         ) extends NonMagicalWeapon {
+
+  def this(name: String, attack: Int, weight: Int) = {
+    this(name, attack, weight, null)
+  }
+}
+
+/**
+ *
+ * @param name   The name of the Sword
+ * @param attack The attack value
+ * @param weight The Sword's weight
+ * @param owner  The Sword's owner
+ */
+class Bow(
+           val name: String,
+           val attack: Int,
+           val weight: Int,
+           var owner: Character,
+         ) extends NonMagicalWeapon {
+
+  def this(name: String, attack: Int, weight: Int) = {
+    this(name, attack, weight, null)
+  }
+}
+
+/**
+ *
+ * @param name        The name of the Sword
+ * @param attack      The attack value
+ * @param weight      The Sword's weight
+ * @param magicAttack The magic attack value
+ * @param owner       The Sword's owner
+ */
+class Wand (
+             val name: String,
+             val attack: Int,
+             val weight: Int,
+             val magicAttack: Int,
+             var owner: Character,
+           ) extends MagicalWeapon {
+
+  def this(name: String, attack: Int, weight: Int, magicAttack: Int) = {
+    this(name, attack, weight, magicAttack, null)
+  }
+}
+
+/**
+ *
+ * @param name        The name of the Sword
+ * @param attack      The attack value
+ * @param weight      The Sword's weight
+ * @param magicAttack The magic attack value
+ * @param owner       The Sword's owner
+ */
+class Staff (
+             val name: String,
+             val attack: Int,
+             val weight: Int,
+             val magicAttack: Int,
+             var owner: Character,
+           ) extends MagicalWeapon {
+
+  def this(name: String, attack: Int, weight: Int, magicAttack: Int) = {
+    this(name, attack, weight, magicAttack, null)
   }
 }
