1fdda46979fd3095372779350d6239b393945599
Ateuluz <lucas.zulueta.w@gmail.com>
2024-03-28 23:15:30 -0300

    Adding first test cases

--- src/test/scala/elementsTest/charactersTest.scala
+++ src/test/scala/elementsTest/charactersTest.scala
@@ -1,11 +1,6 @@
 package elementsTest
 
-import elements.Paladin
-import elements.Warrior
-import elements.Ninja
-import elements.WhiteMage
-import elements.DarkMage
-import elements.Weapon
+import elements.{BlackMage, Ninja, Paladin, Sword, Warrior, Weapon, WhiteMage}
 
 //import org.junit.*
 //import org.junit.Assert.*
@@ -13,15 +8,23 @@ import elements.Weapon
 class PaladinTest extends munit.FunSuite{
   var tstSbjt1: Paladin = _
   var tstSbjt2: Paladin = _
+  var tstObjt1: Sword   = _
 
   override def beforeEach(context: BeforeEach): Unit = {
     //super.beforeEach(context)
+    tstObjt1 = new Sword("Excal", 10, 20, tstSbjt2)
     tstSbjt1 = new Paladin("John", 80, 20, 5)
-    tstSbjt2 = new Paladin("Doe", 200, 10, 25)
+    tstSbjt2 = new Paladin("Doe", 200, 10, 25, tstObjt1)
   }
 
-  test("???") {
-    val expected = ???
+  test("Weapon Ownership") {
+    val expected: Sword = tstObjt1
+    assertEquals(tstSbjt2.weapon,expected,"Weapon should be the given")
+  }
+
+  test("Null Weapon Ownership") {
+    val expected: Weapon = null
+    assertEquals(tstSbjt1.weapon,expected,"Weapon should be null")
   }
 }
 
