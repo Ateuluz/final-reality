9775e044b962cab7d1fb3ed27c85b38d3ec33c29
Ateuluz <lucas.zulueta.w@gmail.com>
2024-03-29 00:39:58 -0300

    Test for WhiteMage and Weapon User Check

--- src/test/scala/elementsTest/charactersTest.scala
+++ src/test/scala/elementsTest/charactersTest.scala
@@ -1,6 +1,6 @@
 package elementsTest
 
-import elements.{BlackMage, Ninja, Paladin, Sword, Warrior, Weapon, WhiteMage}
+import elements.{BlackMage, Ninja, Paladin, Sword, SwordBearer, Wand, WandUser, Warrior, Weapon, WhiteMage}
 
 class PaladinTest extends munit.FunSuite{
   var tstSbjt1: Paladin = _
@@ -95,5 +95,29 @@ class BlackMageTest extends munit.FunSuite{
 }
 
 class WhiteMageTest extends munit.FunSuite{
+  var tstSbjt1: WhiteMage = _
+  var tstSbjt2: WhiteMage = _
+  var tstObjt1: Wand   = _
 
+  override def beforeEach(context: BeforeEach): Unit = {
+    //super.beforeEach(context)
+    tstObjt1 = new Wand("Excal", 10, 20, 10, tstSbjt2)
+    tstSbjt1 = new WhiteMage("John", 80, 20, 5)
+    tstSbjt2 = new WhiteMage("Doe", 200, 10, 25, tstObjt1)
+  }
+
+  test("Weapon User") {
+    assertEquals(tstSbjt1.isInstanceOf[SwordBearer],false,"Shouldn't be allowed")
+    assertEquals(tstSbjt1.isInstanceOf[WandUser],true,"Should be allowed")
+  }
+
+  test("Weapon Ownership") {
+    val expected: Wand = tstObjt1
+    assertEquals(tstSbjt2.weapon,expected,"Weapon should be the given")
+  }
+
+  test("Null Weapon Ownership") {
+    val expected: Weapon = null
+    assertEquals(tstSbjt1.weapon,expected,"Weapon should be null")
+  }
 }
