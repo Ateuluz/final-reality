4d6cba0f7b673d382c97146d8f525bcf98c7634c
Ateuluz <lucas.zulueta.w@gmail.com>
2024-03-29 00:20:19 -0300

    Sword test structure to Ninja

--- src/test/scala/elementsTest/charactersTest.scala
+++ src/test/scala/elementsTest/charactersTest.scala
@@ -49,7 +49,26 @@ class WarriorTest extends munit.FunSuite{
 }
 
 class NinjaTest extends munit.FunSuite{
+  var tstSbjt1: Ninja = _
+  var tstSbjt2: Ninja = _
+  var tstObjt1: Sword   = _
 
+  override def beforeEach(context: BeforeEach): Unit = {
+    //super.beforeEach(context)
+    tstObjt1 = new Sword("Excal", 10, 20, tstSbjt2)
+    tstSbjt1 = new Ninja("John", 80, 20, 5)
+    tstSbjt2 = new Ninja("Doe", 200, 10, 25, tstObjt1)
+  }
+
+  test("Weapon Ownership") {
+    val expected: Sword = tstObjt1
+    assertEquals(tstSbjt2.weapon,expected,"Weapon should be the given")
+  }
+
+  test("Null Weapon Ownership") {
+    val expected: Weapon = null
+    assertEquals(tstSbjt1.weapon,expected,"Weapon should be null")
+  }
 }
 
 class DarkMageTest extends munit.FunSuite{
