a30018f2d3f0324753cd2f834f88eaeeed7dcdcf
Ateuluz <lucas.zulueta.w@gmail.com>
2024-03-30 03:14:04 -0300

    fix(teams): Simplified addCharacter

--- src/main/scala/elements/teams.scala
+++ src/main/scala/elements/teams.scala
@@ -6,12 +6,11 @@ class Party {
   val characters: Array[Character] = Array.ofDim[Character](3)
 
   def addCharacter(character: Character): Unit = {
-    if (this.characters(0) == null) {
-      this.characters(0) = character
-    } else if (this.characters(1) == null) {
-      this.characters(1) = character
-    } else if(this.characters(2) == null) {
-      this.characters(2) = character
+    for (slot <- 0 until this.characters.length){
+      if (this.characters(slot) == null) {
+        this.characters(slot) = character
+        return
+      }
     }
   }
 
@@ -24,10 +23,10 @@ class Enemies(quantity: Int) {
   val characters: Array[Enemy] = Array.ofDim[Enemy](quantity)
 
   def addEnemy(enemy: Enemy): Unit = {
-    for (slot <- 0 to this.characters.length){
+    for (slot <- 0 until this.characters.length){
       if (this.characters(slot) == null) {
         this.characters(slot) = enemy
-        break
+        return
       }
     }
   }
