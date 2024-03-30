7bc8af8dc8856ce8464b6089903a6d82a160822d
Ateuluz <lucas.zulueta.w@gmail.com>
2024-03-30 02:57:54 -0300

    feat(teams): Enemies can be added to the enemy party

--- src/main/scala/elements/teams.scala
+++ src/main/scala/elements/teams.scala
@@ -1,5 +1,7 @@
 package elements
 
+import scala.util.control.Breaks.break
+
 class Party {
   val characters: Array[Character] = Array.ofDim[Character](3)
 
@@ -20,4 +22,13 @@ class Party {
 
 class Enemies(quantity: Int) {
   val characters: Array[Enemy] = Array.ofDim[Enemy](quantity)
+
+  def addEnemy(enemy: Enemy): Unit = {
+    for (slot <- 0 to this.characters.length){
+      if (this.characters(slot) == null) {
+        this.characters(slot) = enemy
+        break
+      }
+    }
+  }
 }
