befd8270caa2e7007fac4cc0aa99631464fdebb8
Ateuluz <lucas.zulueta.w@gmail.com>
2024-04-21 18:32:45 -0400

    refactor: WandTest / AxeTest <- armamentTest

--- src/test/scala/modelTest/armamentTest/axeTest/AxeTest.scala
+++ src/test/scala/modelTest/armamentTest/axeTest/AxeTest.scala
@@ -1,5 +1,27 @@
 package modelTest.armamentTest.axeTest
 
-class AxeTest {
+import model.armament.axe.Axe
+import model.characters.AxeBearer
+import model.characters.paladin.Paladin
 
+class AxeTest extends munit.FunSuite() {
+  var ch1: AxeBearer = _
+  var wp1: Axe = _
+  var wp2: Axe = _
+
+  override def beforeEach(context: BeforeEach): Unit = {
+    //super.beforeEach(context)
+    ch1 = new Paladin("A", 10, 10, 10, wp1)
+    wp1 = new Axe("B", 5, 10, ch1)
+    wp2 = new Axe("C", 20, 30)
+  }
+
+  test("Owner Check") {
+    val expected1 = ch1
+    val actual1   = wp1.owner
+      assertEquals(actual1,expected1)
+    val expected2: AxeBearer = null
+    val actual2              = wp2.owner
+      assertEquals(actual2,expected2)
+  }
 }
--- src/test/scala/modelTest/armamentTest/wandTest/WandTest.scala
+++ src/test/scala/modelTest/armamentTest/wandTest/WandTest.scala
@@ -1,5 +1,27 @@
 package modelTest.armamentTest.wandTest
 
-class WandTest {
+import model.armament.wand.Wand
+import model.characters.WandUser
+import model.characters.whiteMage.WhiteMage
 
+class WandTest extends munit.FunSuite() {
+  var ch1: WandUser = _
+  var wp1: Wand = _
+  var wp2: Wand = _
+
+  override def beforeEach(context: BeforeEach): Unit = {
+    //super.beforeEach(context)
+    ch1 = new WhiteMage("A", 10, 10, 10, 10, wp1)
+    wp1 = new Wand("B", 5, 10, 5, ch1)
+    wp2 = new Wand("C", 20, 30, 10)
+  }
+
+  test("Owner Check") {
+    val expected1 = ch1
+    val actual1   = wp1.owner
+    assertEquals(actual1,expected1)
+    val expected2: WandUser = null
+    val actual2             = wp2.owner
+    assertEquals(actual2,expected2)
+  }
 }
