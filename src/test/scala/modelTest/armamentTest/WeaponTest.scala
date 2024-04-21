1ace39ee1a60fd2a3fd120232b495ef76649af51
Ateuluz <lucas.zulueta.w@gmail.com>
2024-04-21 18:52:29 -0400

    fix: armamentTest to WeaponTest without redundant tests

---
+++ src/test/scala/modelTest/armamentTest/WeaponTest.scala
@@ -0,0 +1,41 @@
+package modelTest.armamentTest
+
+import model.armament.staff.Staff
+import model.armament.sword.Sword
+
+class WeaponTest extends munit.FunSuite{
+  var wp1: Sword = _
+  var wp2: Staff = _
+
+  override def beforeEach(context: BeforeEach): Unit = {
+    wp1 = new Sword("A", 5, 10)
+    wp2 = new Staff("B", 20, 30, 25)
+  }
+
+  test("Limited Variable Ranges") {
+    var allowNegativeHP: Boolean = false
+    var allowNegativeWeight: Boolean = false
+    var allowNegativeMagicAttack: Boolean = false
+    try {
+      wp1 = new Sword("A", -1, 1)
+      allowNegativeHP = true
+    } catch {
+      case _: java.lang.IllegalArgumentException =>
+    }
+    try {
+      wp1 = new Sword("A", 1, -1)
+      allowNegativeWeight = true
+    } catch {
+      case _: java.lang.IllegalArgumentException =>
+    }
+    try {
+      wp2 = new Staff("B", 1, 1, -1)
+      allowNegativeMagicAttack = true
+    } catch {
+      case _: java.lang.IllegalArgumentException =>
+    }
+    assertEquals(allowNegativeHP,false,"Negative HP shouldn't be allowed")
+    assertEquals(allowNegativeWeight,false,"Negative or zero Weight shouldn't be allowed") // Speed related
+    assertEquals(allowNegativeMagicAttack,false,"Negative MagicAttack shouldn't be allowed")
+  }
+}
