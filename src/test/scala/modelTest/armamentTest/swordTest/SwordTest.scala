e7d45b8677d9ec59759f9ac01d5a6f8c9420296b
Ateuluz <lucas.zulueta.w@gmail.com>
2024-04-21 18:35:30 -0400

    refactor: SwordTest <- armamentTest

--- src/test/scala/modelTest/armamentTest/swordTest/SwordTest.scala
+++ src/test/scala/modelTest/armamentTest/swordTest/SwordTest.scala
@@ -1,5 +1,27 @@
 package modelTest.armamentTest.swordTest
 
-class SwordTest {
+import model.armament.sword.Sword
+import model.characters.SwordBearer
+import model.characters.warrior.Warrior
 
+class SwordTest extends munit.FunSuite() {
+  var ch1: SwordBearer = _
+  var wp1: Sword = _
+  var wp2: Sword = _
+
+  override def beforeEach(context: BeforeEach): Unit = {
+    //super.beforeEach(context)
+    ch1 = new Warrior("A", 10, 10, 10, wp1)
+    wp1 = new Sword("B", 5, 10, ch1)
+    wp2 = new Sword("C", 20, 30)
+  }
+
+  test("Owner Check") {
+    val expected1 = ch1
+    val actual1   = wp1.owner
+    assertEquals(actual1,expected1)
+    val expected2: SwordBearer = null
+    val actual2                = wp2.owner
+    assertEquals(actual2,expected2)
+  }
 }
