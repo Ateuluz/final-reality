93ed66a64fbd2478d38f25da5126e05333ffe105
Ateuluz <lucas.zulueta.w@gmail.com>
2024-04-21 23:41:32 -0400

    refactor: charactersTest |> CharacterTest

---
+++ src/test/scala/modelTest/charactersTest/CharacterTest.scala
@@ -0,0 +1,60 @@
+package modelTest.charactersTest
+
+import model.armament.Weapon
+import model.armament.sword.Sword
+import model.armament.wand.Wand
+import model.characters._
+import model.characters.blackMage.BlackMage
+import model.characters.ninja.Ninja
+import model.characters.paladin.Paladin
+import model.characters.warrior.Warrior
+import model.characters.whiteMage.WhiteMage
+
+class CharacterTest extends munit.FunSuite{
+  var tstSbjt1: Paladin   = _
+  var tstSbjt2: WhiteMage = _
+  var tstSbjt3: Warrior   = _
+  var tstObjt1: Sword     = _
+
+  override def beforeEach(context: BeforeEach): Unit = {
+    //super.beforeEach(context)
+    tstObjt1 = new Sword("Excal", 10, 20, tstSbjt1)
+    tstSbjt1 = new Paladin("John", 80, 20, 5, tstObjt1)
+    tstSbjt2 = new WhiteMage("Doe", 200, 10, 25, 150)
+  }
+
+  test("Limited Variable Ranges") {
+    var allowNegativeHP: Boolean = false
+    var allowNegativeDefense: Boolean = false
+    var allowNegativeWeight: Boolean = false
+    var allowNegativeMana: Boolean = false
+    try {
+      tstSbjt3 = new Warrior("Joe", -1, 1, 1)
+      allowNegativeHP = true
+    } catch {
+      case _: java.lang.IllegalArgumentException =>
+    }
+    try {
+      tstSbjt3 = new Warrior("Joe", 1, -1, 1)
+      allowNegativeDefense = true
+    } catch {
+      case _: java.lang.IllegalArgumentException =>
+    }
+    try {
+      tstSbjt3 = new Warrior("Joe", 1, 1, -1)
+      allowNegativeWeight = true
+    } catch {
+      case _: java.lang.IllegalArgumentException =>
+    }
+    try {
+      tstSbjt2 = new WhiteMage("Joe", 1, 1, 1, -1)
+      allowNegativeMana = true
+    } catch {
+      case _: java.lang.IllegalArgumentException =>
+    }
+    assertEquals(allowNegativeHP,false,"Negative HP shouldn't be allowed")
+    assertEquals(allowNegativeDefense,false,"Negative Defense shouldn't be allowed")
+    assertEquals(allowNegativeWeight,false,"Negative or zero Weight shouldn't be allowed") // Speed related
+    assertEquals(allowNegativeMana,false,"Negative Mana shouldn't be allowed")
+  }
+}
