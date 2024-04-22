87e11e4cede8eaded38559d272819b81909337b3
Ateuluz <lucas.zulueta.w@gmail.com>
2024-04-22 11:27:14 -0400

    refactor(teams.enemies.Enemies): Update loop instructions in 'for' loop

--- src/main/scala/model/teams/enemies/Enemies.scala
+++ src/main/scala/model/teams/enemies/Enemies.scala
@@ -6,7 +6,7 @@ class Enemies (quantity: Int) {
   val characters: Array[Enemy] = Array.ofDim[Enemy](quantity)
 
   def addEnemy(enemy: Enemy): Unit = {
-    for (slot <- 0 until this.characters.length){
+    for (slot <- this.characters.indices){
       if (this.characters(slot) == null) {
         this.characters(slot) = enemy
         return
