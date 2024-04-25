5c7a9f7f6fbc3c6e8006de7ced12916f62495442
Ateuluz <lucas.zulueta.w@gmail.com>
2024-04-25 16:56:38 -0400

    feat(enemies): Enemies can now attack

--- src/main/scala/model/entities/enemies/AEnemy.scala
+++ src/main/scala/model/entities/enemies/AEnemy.scala
@@ -1,6 +1,6 @@
 package model.entities.enemies
 
-import model.entities.AEntity
+import model.entities.{AEntity, IEntity}
 
 abstract class AEnemy (
                         name: String,
@@ -11,11 +11,13 @@ abstract class AEnemy (
                       ) extends AEntity(name,hp,defense,weight)
                           with IEnemy {
   private val _attack: Int = constrainAttack(attack)
-  def getAttack: Int = _attack
-  def constrainAttack(attack: Int): Int = {
+  override def getAttack: Int = _attack
+  override def constrainAttack(attack: Int): Int = {
     attack match {
       case n if n <= 0 => 1
       case _ => attack
     }
   }
+  override def attack(objective: IEntity): Int =
+    objective.defend(_attack)
 }
--- src/main/scala/model/entities/enemies/IEnemy.scala
+++ src/main/scala/model/entities/enemies/IEnemy.scala
@@ -4,5 +4,5 @@ import model.entities.IEntity
 
 trait IEnemy extends IEntity {
   def getAttack: Int
-  def constrainAttack(attack: Int): Int
+  protected def constrainAttack(attack: Int): Int
 }
