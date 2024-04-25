ff7e75f5a3adf9bedc3d13c61864436f977f5996
Ateuluz <lucas.zulueta.w@gmail.com>
2024-04-25 15:35:39 -0400

    test(EntityTest): Defined test for attacking
    The attack method should call the protected defend method of an entity with an attack input, the receiving counterpart should substract its defense from the incoming attack to get a raw damage, constrain it and substract it from its healthpoints. Note: Remember to restrain a dead entity from taking turns

--- src/test/scala/modelTest/entitiesTest/EntityTest.scala
+++ src/test/scala/modelTest/entitiesTest/EntityTest.scala
@@ -1,11 +1,17 @@
 package modelTest.entitiesTest
 
+import model.armament.sword.Sword
 import model.entities.characters.paladin.Paladin
 
 class EntityTest extends munit.FunSuite {
   var ch1: Paladin = _
+  var ch2: Paladin = _
+  var wp1: Sword   = _
   override def beforeEach(context: BeforeEach): Unit = {
     ch1 = new Paladin("A", 1, 1, 1)
+    ch2 = new Paladin("B", 2, 2, 2)
+    wp1 = new Sword("C", 3, 3)
+    ch1.requestBindWeapon(wp1)
   }
 
   test("Have name") {
@@ -17,4 +23,8 @@ class EntityTest extends munit.FunSuite {
     assertEquals(ch1.getHp,2)
   }
 
+  test("Attack, Defend and NonNegative Health") {
+    ch2.attack(ch1)
+    assertEquals(ch1.getHp, 0)
+  }
 }
