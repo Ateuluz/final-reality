669133797a266d191b5969c6b716a26b74cd3358
Ateuluz <lucas.zulueta.w@gmail.com>
2024-04-25 15:41:52 -0400

    test(EntityTest): Added a test for when defense is grater than attack
    Should defense be greater than the incoming attack, no damage should be taken, and no health points should be gained, thus remaining the same

--- src/test/scala/modelTest/entitiesTest/EntityTest.scala
+++ src/test/scala/modelTest/entitiesTest/EntityTest.scala
@@ -1,5 +1,6 @@
 package modelTest.entitiesTest
 
+import model.armament.sword
 import model.armament.sword.Sword
 import model.entities.characters.paladin.Paladin
 
@@ -7,11 +8,14 @@ class EntityTest extends munit.FunSuite {
   var ch1: Paladin = _
   var ch2: Paladin = _
   var wp1: Sword   = _
+  var wp2: Sword   = _
   override def beforeEach(context: BeforeEach): Unit = {
     ch1 = new Paladin("A", 1, 1, 1)
     ch2 = new Paladin("B", 2, 2, 2)
-    wp1 = new Sword("C", 3, 3)
+    wp1 = new Sword("C", 1, 1)
+    wp2 = new Sword("D", 3, 3)
     ch1.requestBindWeapon(wp1)
+    ch2.requestBindWeapon(wp2)
   }
 
   test("Have name") {
@@ -24,6 +28,8 @@ class EntityTest extends munit.FunSuite {
   }
 
   test("Attack, Defend and NonNegative Health") {
+    ch1.attack(ch2)
+    assertEquals(ch2.getHp, 2)
     ch2.attack(ch1)
     assertEquals(ch1.getHp, 0)
   }
