d683dfa41549daf0aa67a547646517e2b28e59d5
Ateuluz <lucas.zulueta.w@gmail.com>
2024-05-08 14:46:39 -0400

    docs(enemy): Added simple docs to methods of IEnemy

--- src/main/scala/model/entities/enemies/IEnemy.scala
+++ src/main/scala/model/entities/enemies/IEnemy.scala
@@ -3,5 +3,9 @@ package model.entities.enemies
 import model.entities.IEntity
 
 trait IEnemy extends IEntity {
+  /**
+   *
+   * @return The attack stat of this entity
+   */
   def getAttack: Int
 }
