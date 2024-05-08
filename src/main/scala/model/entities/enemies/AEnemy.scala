456bea986dbc78556dbbc970d91d79096b113c05
Ateuluz <lucas.zulueta.w@gmail.com>
2024-05-08 14:55:42 -0400

    docs(AEnemy): Added simple docs to constraining methods

--- src/main/scala/model/entities/enemies/AEnemy.scala
+++ src/main/scala/model/entities/enemies/AEnemy.scala
@@ -14,12 +14,17 @@ abstract class AEnemy (
   private val _attack: Int = constrainAttack(attack)
   Require.Stat(attack, "Attack") atLeast 1
   override def getAttack: Int = _attack
+  override def attack(objective: IEntity): Int =
+    objective.defend(_attack)
+  /**
+   *
+   * @param attack The original attack value
+   * @return The final valid attack value
+   */
   private def constrainAttack(attack: Int): Int = {
     attack match {
       case n if n < 1 => 1
       case _ => attack
     }
   }
-  override def attack(objective: IEntity): Int =
-    objective.defend(_attack)
 }
