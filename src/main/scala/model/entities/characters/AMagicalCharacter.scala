f07d1ba6fee0e5d4d40a3d31b1a048ba52f05777
Ateuluz <lucas.zulueta.w@gmail.com>
2024-05-08 14:59:15 -0400

    docs(AMagicalCharacter): Added simple docs to constraining methods

--- src/main/scala/model/entities/characters/AMagicalCharacter.scala
+++ src/main/scala/model/entities/characters/AMagicalCharacter.scala
@@ -16,6 +16,11 @@ abstract class AMagicalCharacter(
   override def setMana(mana: Int): Unit = {
     _mana = constrainMana(mana)
   }
+  /**
+   *
+   * @param mana The original mana value
+   * @return The final valid mana value
+   */
   private def constrainMana(mana: Int): Int = {
     mana match {
       case n if n < 1 => 1
