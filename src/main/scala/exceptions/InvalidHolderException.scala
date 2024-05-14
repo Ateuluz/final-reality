ac05aa4666000bd1dc0aef141f5e5dcc733b0924
Ateuluz <lucas.zulueta.w@gmail.com>
2024-05-14 19:37:33 -0400

    feat(InvalidHolderException): Created exception to handle invalid weapon assignations.

---
+++ src/main/scala/exceptions/InvalidHolderException.scala
@@ -0,0 +1,19 @@
+package exceptions
+
+/**
+ * Custom exception to signal an invalid weapon assignation encountered.
+ *
+ * This exception is designed to provide more specific feedback about
+ * invalid weapon assignations. For instance, if a character is unable to equip
+ * a weapon, this exception could be thrown with a detailed
+ * message indicating the nature of the problem.
+ *
+ * @example To throw the exception with specific details:
+ * {{{
+ * throw new InvalidHolderException("Paladin character cannot equip this type of weapon.")
+ * // => InvalidHolderException: An invalid holder was found -- Paladin character cannot equip this type of weapon.
+ * }}}
+ *
+ * @param details A descriptive message detailing the nature of the invalid stat.
+ */
+class InvalidHolderException(details: String) extends Exception(s"An invalid holder was found -- $details")
