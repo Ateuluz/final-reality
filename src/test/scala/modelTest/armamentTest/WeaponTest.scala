df7e5c1d5798865f9cd38f43277aa57ead58d64b
Ateuluz <lucas.zulueta.w@gmail.com>
2024-05-15 00:33:32 -0400

    test(WeaponTest): Test for invalid weapon assignations

--- src/test/scala/modelTest/armamentTest/WeaponTest.scala
+++ src/test/scala/modelTest/armamentTest/WeaponTest.scala
@@ -1,8 +1,16 @@
 package modelTest.armamentTest
 
-import exceptions.InvalidStatException
+import exceptions.{InvalidHolderException, InvalidStatException}
+import model.armament.axe.Axe
+import model.armament.bow.Bow
 import model.armament.staff.Staff
 import model.armament.sword.Sword
+import model.armament.wand.Wand
+import model.entities.characters.blackMage.BlackMage
+import model.entities.characters.ninja.Ninja
+import model.entities.characters.paladin.Paladin
+import model.entities.characters.warrior.Warrior
+import model.entities.characters.whiteMage.WhiteMage
 
 class WeaponTest extends munit.FunSuite{
   var wp1: Sword = _
@@ -76,5 +84,50 @@ class WeaponTest extends munit.FunSuite{
 
   test("Has MagicAttack") {
     assertEquals(wp2.getMagicAttack,25)
+  }
+
+  test("Non User Cannot Equip Extended") {
+    val chAux1 = new Warrior("X", 2, 2, 2)
+    val chAux2 = new Paladin("X", 2, 2, 2)
+    val chAux3 = new Ninja("X", 2, 2, 2)
+    val chAux4 = new WhiteMage("X", 2, 2, 2, 2)
+    val chAux5 = new BlackMage("X", 2, 2, 2, 2)
+
+    val wpAux1 = new Sword("X", 2, 2)
+    val wpAux2 = new Axe("X", 2, 2)
+    val wpAux3 = new Bow("X", 2, 2)
+    val wpAux4 = new Wand("X", 2, 2, 2)
+    val wpAux5 = new Staff("X", 2, 2, 2)
+
+    try {
+      chAux4.equip(wpAux1)
+    } catch {
+      case _: InvalidHolderException =>
+      case _ => fail("The Weapon Is Not Supposed To Be Assigned")
+    }
+    try {
+      chAux3.equip(wpAux2)
+    } catch {
+      case _: InvalidHolderException =>
+      case _ => fail("The Weapon Is Not Supposed To Be Assigned")
+    }
+    try {
+      chAux5.equip(wpAux3)
+    } catch {
+      case _: InvalidHolderException =>
+      case _ => fail("The Weapon Is Not Supposed To Be Assigned")
+    }
+    try {
+      chAux1.equip(wpAux4)
+    } catch {
+      case _: InvalidHolderException =>
+      case _ => fail("The Weapon Is Not Supposed To Be Assigned")
+    }
+    try {
+      chAux2.equip(wpAux5)
+    } catch {
+      case _: InvalidHolderException =>
+      case _ => fail("The Weapon Is Not Supposed To Be Assigned")
+    }
   }
 }
