84cbe7fd7a8e3f94cab6fa1007a7080ceb88c7ff
Ateuluz <lucas.zulueta.w@gmail.com>
2024-05-15 00:00:11 -0400

    fix(Bow): Elimination of obsolete method.

--- src/main/scala/model/armament/bow/Bow.scala
+++ src/main/scala/model/armament/bow/Bow.scala
@@ -17,17 +17,6 @@ class Bow(
            attack: Int,
            weight: Int,
          ) extends AWeapon(name,attack,weight) {
-  /**
-   *
-   * @param character the character we intend to equip the weapon to
-   *  @return Boolean representing if the character can equip it
-   */
-  override protected[model] def canBeEquippedBy(character: ICharacter): Boolean = {
-    character match {
-      case _: IBowBearer => getOwner.isEmpty
-      case _ => false
-    }
-  }
 
   override def equipToWhiteMage(whiteMage: WhiteMage): Unit =
     setOwner(whiteMage)
