2cebb0b119b7c2a7f1e88f73bd4c8a082c78b472
Ateuluz <lucas.zulueta.w@gmail.com>
2024-05-18 02:45:11 -0400

    refactor(IEnemy): removed method defined on superclass

--- src/main/scala/model/entities/enemies/IEnemy.scala
+++ src/main/scala/model/entities/enemies/IEnemy.scala
@@ -6,9 +6,4 @@ import model.entities.IEntity
  * Trait representing an enemy and its methods
  */
 trait IEnemy extends IEntity {
-  /**
-   *
-   * @return The attack stat of this entity
-   */
-  def getAttack: Int
 }
