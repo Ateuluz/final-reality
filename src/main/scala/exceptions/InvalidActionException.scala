3e86248ab73bacfdfd20d54947c57d13a7625cd8
Ateuluz <lucas.zulueta.w@gmail.com>
2024-05-18 02:43:08 -0400

    feat(InvalidActionException): Created Exception Handler for invalid entity actions

---
+++ src/main/scala/exceptions/InvalidActionException.scala
@@ -0,0 +1,19 @@
+package exceptions
+
+/**
+ * Custom exception to signal an invalid action within game has been made.
+ *
+ * This exception is designed to provide more specific feedback about
+ * invalid actions entities could make. For instance, if a character is unable
+ * to attack an ally, this exception could be thrown with a detailed
+ * message indicating the nature of the problem.
+ *
+ * @example To throw the exception with specific details:
+ * {{{
+ * throw new InvalidActionException("Character cannot attack this entity.")
+ * // => InvalidHolderException: An invalid action was found -- Character cannot attack this entity.
+ * }}}
+ *
+ * @param details A descriptive message detailing the nature of the invalid action.
+ */
+class InvalidActionException(details: String) extends Exception(s"An invalid action was found -- $details")
