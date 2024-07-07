6d693edf34616109e675a9fa690c9bcfb8227073
Ateuluz <lucas.zulueta.w@gmail.com>
2024-07-07 19:03:17 -0400

    refactor(controller): Changes to fit new design

---
+++ src/main/scala/controller/states/AGameState.scala
@@ -0,0 +1,91 @@
+package controller.states
+
+import controller.{IGameController, IGameState}
+import exceptions.InvalidStateException
+import model.entities.IEntity
+import model.entities.enemies.IEnemy
+import model.spells.ISpell
+
+import scala.collection.mutable.ArrayBuffer
+
+/** Ateuluz
+ *
+ * An abstract class for defining Game states and their methods
+ *
+ * @param controller The associated controller for the state
+ */
+abstract class AGameState (
+                            val controller: IGameController
+                          ) extends IGameState {
+
+  /** Ateuluz
+   *
+   * To improve readability
+   */
+  private def raiseException(): Unit = throw new InvalidStateException("This State cannot handle the intended action")
+
+  /** Ateuluz
+   *
+   * Step method to handle game flow
+   */
+  override def step(): Unit = raiseException()
+
+  /** Ateuluz
+   *
+   * @param entities Potential targets
+   * @return A string to pass as prompt
+   */
+  protected def mapEntitiesToIndexedString(entities: ArrayBuffer[_ <: IEntity]): String = {
+    entities.zipWithIndex.map { case (entity, index) =>
+      s"${index + 1}: ${entity.getName}"
+    }.mkString("\n")
+  }
+
+  /** Ateuluz
+   *
+   * @param entities Where to choose from
+   * @return Target
+   */
+  protected def chooseTargetGeneric(entities: ArrayBuffer[_ <: IEntity]): IEntity = {
+    val entitiesList = mapEntitiesToIndexedString(entities)
+    val prompt = s"Choose your target:\n$entitiesList"
+    val opt = controller.getInput(prompt)
+    try {
+      val index = opt.toInt - 1
+      if (index >= 0 && index < entities.length) {
+        entities(index)
+      } else {
+        println("Invalid choice. Please choose a valid target number.")
+        chooseTargetGeneric(entities)
+      }
+    } catch {
+      case _: NumberFormatException =>
+        println("Invalid input. Please enter a number.")
+        chooseTargetGeneric(entities)
+    }
+  }
+
+  protected def chooseSpell(spells: ArrayBuffer[ISpell]): Int = {
+    val spellsList = spells.zipWithIndex.map { case (spell, index) =>
+      s"${index + 1}: ${spell.getClass.getSimpleName}"
+    }.mkString("\n")
+
+    val prompt = s"Choose a spell:\n$spellsList"
+    val opt = controller.getInput(prompt)
+
+    try {
+      val spellID = opt.toInt - 1
+      if (spellID >= 0 && spellID < spells.length) {
+        spellID
+      } else {
+        println("Invalid choice. Please choose a valid spell number.")
+        chooseSpell(spells)
+      }
+    } catch {
+      case _: NumberFormatException =>
+        println("Invalid input. Please enter a number.")
+        chooseSpell(spells)
+    }
+  }
+
+}
