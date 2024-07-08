eb836f06d5cbabee38321f79600a8b036bca8843
Ateuluz <lucas.zulueta.w@gmail.com>
2024-07-08 11:34:49 -0400

    refactor: Action Bar Raise is now automatic
    Previously, all bifurcations within the transition state awaited for a player interaction, now raising action bars will be done automatically.

--- src/main/scala/controller/states/STransition.scala
+++ src/main/scala/controller/states/STransition.scala
@@ -6,15 +6,24 @@ class STransition(
                    controller: IGameController
                  ) extends AGameState(controller) {
 
+  private def waitForResponse(): Unit =
+    if (!controller.isTesting) {
+      controller.getInput("[ENTER TO CONTINUE]")
+      println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+      println("==============================================")
+    }
+
   override def step(): Unit = {
     println("")
     if (controller.turnScheduler.party.get.isDefeated) {
       println("You've been defeated! Too bad.")
       controller.state = GameStateFactory.createState("End",controller)
+      waitForResponse()
     }
     else if (controller.turnScheduler.enemyTeam.get.isDefeated) {
       println("You won! Lucky you.")
       controller.state = GameStateFactory.createState("End",controller)
+      waitForResponse()
     }
     else if (controller.turnScheduler.charactersFull.isEmpty) {
       println("No entities ready! Raise action bars!")
@@ -25,12 +34,9 @@ class STransition(
       println(s"\n>>> Turn Phase for ${ent.getName}! [HP: ${ent.getHp}/${ent.getHpMax}]")
       controller.turnScheduler.atTurn.actionAble = true
       controller.state = GameStateFactory.createState("Apply Effects", controller)
+      waitForResponse()
     }
-    if (!controller.isTesting) {
-      controller.getInput("[ENTER TO CONTINUE]")
-      println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
-      println("==============================================")
-    }
+
     controller.step()
   }
 }
